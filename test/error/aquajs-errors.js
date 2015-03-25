455a9b1ccaa83a3f25cc356044045f09ee57d054
Priyadarshini Sarode <psarode@equinix.com>
2015-03-25 10:03:42 -0700

    Added more tests

---
+++ test/error/aquajs-errors.js
@@ -0,0 +1,31 @@
+"use strict";
+var path = require('path'),
+    aquajsErrorConstant = require(path.join($dirPaths.serverDir,'config','env','error-constants.json')),
+    extendedErrorConstant = require(path.join($dirPaths.serverDir,'config','env','extended-error-constants'));
+var AquaJsErrors = function () {
+  var errors = [];
+  this.errors = errors;
+
+};
+/**
+ * AquaJS Error Constructor to list all the errors
+ * Lets say you wanted to send multiple errors to the browser
+ * you can push the list of errors to the errors list and pass that list while throwing the aqua js error
+ * Framework will take care of sending the list of error in a json format
+ * @api public
+ * @param
+ * @see
+ * @return {Aquajs Error Object}
+ */
+AquaJsErrors.prototype.addError = function () {
+  this.errors.push(arguments);
+};
+AquaJsErrors.prototype.getErrors = function () {
+  return this.errors;
+};
+AquaJsErrors.prototype.hasError = function () {
+  return this.errors.length > 0;
+};
+
+module.exports = AquaJsErrors;
+
